3c4d7ec8db27710647838ca8f6500c9047cdc889
ebru isleyen <ebruisleyene@gmail.com>
2023-05-12 01:41:47 +0300

    soru cözümü

---
+++ soru1.py
@@ -0,0 +1,17 @@
+# The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+# Mind the input validation.
+
+# Example
+# { 6, 2, 1, 8, 10 } => 16
+# { 1, 1, 11, 2, 3 } => 6
+# Input validation
+# If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+# FUNDAMENTALS
+def sum_array(arr):
+    if arr is None or len(arr)==0 or len(arr)==1:
+        return 0
+    else:
+        return  sum(arr) - max(arr) - min(arr)
+        
